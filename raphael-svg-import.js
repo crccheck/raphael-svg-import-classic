04ef733a36b1d70c416721c6f3a8d5fc254309d9
Ingvar Stepanyan <me@rreverser.com>
2013-07-26 05:03:27 +0300

    Added CSS support; different improvements

--- raphael-svg-import.js
+++ raphael-svg-import.js
@@ -1,174 +1,192 @@
 /*
-* Raphael SVG Import Classic 0.1.3 - Extension to Raphael JS
-* https://github.com/crccheck/raphael-svg-import-classic
+* Raphael SVG Import Classic 0.2.0 - Extension to Raphael JS
+* https://github.com/rreverser/raphael-svg-import-classic
 *
-* Raphael SVG Import Classic Copyright (c) 2012 Chris Chang
+* Raphael SVG Import Classic Copyright (c) 2013 Chris Chang, Ingvar Stepanyan
 * Original Raphael SVG Import Copyright (c) 2009 Wout Fierens
 * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
 *
 */
 
 /* global Raphael */
-Raphael.fn.importSVG = function (svgXML, svgId) {
+Raphael.fn.importSVG = function (svgXML) {
   var myNewSet = this.set();
   var groupSet = {};
   var defaultTextAttr = {
     // stroke: "none"
     "text-anchor": "start"  // raphael defaults to "middle"
   };
-  try {
-    this.parseElement = function(elShape) {
-      // skip text nodes
-      if (elShape.nodeType == 3) {
-        return;
-      }
-      var attr = {"stroke": "transparent", "stroke-width": 0, "fill":"#000"}, i;
-      if (elShape.attributes){
-        for (i = elShape.attributes.length - 1; i >= 0; --i){
-          attr[elShape.attributes[i].name] = elShape.attributes[i].value;
-        }
-      }
-      var shape, style;
-      var shapeName = elShape.nodeName;
-      switch(shapeName) {
-        case "svg":
-        case "g":
-          // pass the id to the first child, parse the children
-          var groupId = elShape.getAttribute('id');
-          var groupClass = elShape.getAttribute('class');
-          if (groupId || groupClass) {
-            var elShapeChildren = elShape.childNodes;
-            for (var i = 0, length = elShapeChildren.length; i < length; i++) {
-              var elShapeChild = elShapeChildren[i];
-              if (groupId) {
-                elShapeChild.setAttribute('data-svg-group', groupId);
-              }
-              if (groupClass) {
-                elShapeChild.setAttribute('class', groupClass);
-              }
-            }
-          }
-          var thisGroup = this.set();
-          for (i = 0, length = elShape.childNodes.length; i < length; ++i) {
-            thisGroup.push(this.parseElement(elShape.childNodes.item(i)));
-          }
 
-          // handle transform attribute
-          if (attr.transform){
-            var match = /translate\(([^,]+),([^,]+)\)/.exec(attr.transform);
-            if (match.length == 3){
-              thisGroup.translate(match[1], match[2]);
-            }
-          }
-
-          // handle display=none
-          if (attr.display === "none") {
-            thisGroup.hide();
-          }
-          // hold onto thisGroup just in case
-          if (groupId && elShape.childNodes.length) {
-            groupSet[groupId] = thisGroup;
-          }
-          return;
-        case "rect":
-          if (attr.rx && attr.ry) {
-            attr.r = (+(attr.rx || 0) + (+(attr.ry || 0))) / 2;
-            delete attr.rx;
-            delete attr.ry;
-          } else {
-            attr.r = attr.rx || attr.ry || 0;
-            delete attr.rx;
-            delete attr.ry;
-          }
-          /* falls through */
-        case "circle":
-        case "ellipse":
-          shape = this[shapeName]();
-        break;
-        case "path":
-          shape = this.path(attr.d);
-          delete attr.d;
-        break;
-        case "polygon":
-          shape = this.polygon(attr);
-        break;
-        case "polyline":
-          shape = this.polyline(attr);
-        break;
-        case "line":
-          shape = this.line(attr);
-        break;
-        case "image":
-          shape = this.image();
-        break;
-        case "text":
-          for (var key in defaultTextAttr){
-            if (!attr[key] && defaultTextAttr.hasOwnProperty(key)) {
-              attr[key] = defaultTextAttr[key];
-            }
-          }
-          shape = this.text(attr.x, attr.y, elShape.text || elShape.textContent);
-        break;
-        default:
-          var elSVG = elShape.getElementsByTagName("svg");
-          if (elSVG.length){
-            elSVG[0].normalize();
-            this.parseElement(elSVG[0]);
-          }
-          return;
-      }
-
-      // apply matrix transformation
-      var matrix = attr.transform;
-      if (matrix) {
-        matrix = matrix.substring(7, matrix.length-1).split(' ')
-                 .map(function(x){ return parseFloat(x); });
-        var m = shape.matrix;
-        m.add.apply(m, matrix);
-        // this seems like a very odd step:
-        shape.transform(m.toTransformString());
-        delete attr.transform;
-      }
-
-      // copy group id
-      shape.node.setAttribute('data-svg-group', elShape.getAttribute('data-svg-group'));
-      var shapeClass = elShape.getAttribute('class');
-      if (shapeClass) {
-        shape.node.setAttribute('class', shape.node.getAttribute('class') + ' ' + shapeClass);
-      }
-      shape.node.setAttribute('data-svg', shapeName);
-
-      shape.attr(attr);
-
-      // assign an arbitrary id
-      var nodeID = elShape.getAttribute("id");
-      if (nodeID) {
-        shape.node.id = nodeID;
-      }
-      
-      myNewSet.push(shape);
-      return shape;
-    };
-
-    this.parseElement(svgXML);
-    
-    var styles = svgXML.getElementsByTagName('style');
-    for (var i = 0, length = styles.length; i < length; i++) {
-      var style = document.createElement('style'), css = styles[i].text;
-      style.setAttribute('type', 'text/css');
-      if (svgId) {
-        style.setAttribute('data-svg-id', svgId);
-      }
-      if (style.styleSheet) {
-        style.styleSheet.cssText = css;
-      } else {
-        style.appendChild(document.createTextNode(css));
-      }
-      document.head.appendChild(style);
+  var forEach = Function.prototype.bind && Array.prototype.forEach ? Function.prototype.call.bind(Array.prototype.forEach) : function (arr, callback) {
+    for (var i = 0, length = arr.length; i < length; i++) {
+      callback(arr[i], i, arr);
     }
-  } catch (error) {
-    throw "SVGParseError (" + error + ")";
-  }
+  };
+
+  this.parseElement = function(elShape) {
+    // skip text nodes
+    if (elShape.nodeType === 3) {
+      return;
+    }
+    var attr = {"stroke": "transparent", "stroke-width": 0, "fill":"#000"}, i;
+    if (elShape.attributes){
+      forEach(elShape.attributes, function (currentAttr) {
+        attr[currentAttr.name] = currentAttr.value;
+      });
+    }
+    var shape;
+    var shapeName = elShape.nodeName;
+    switch(shapeName) {
+      case "svg":
+      case "g":
+        var groupId = elShape.getAttribute('id');
+        var groupClass = elShape.getAttribute('class');
+        if (groupId || groupClass) {
+          var elShapeChildren = elShape.childNodes;
+          for (var i = 0, length = elShapeChildren.length; i < length; i++) {
+            var elShapeChild = elShapeChildren[i];
+            if (elShapeChild.nodeType === 3) continue;
+            if (groupId) {
+              elShapeChild.setAttribute('data-svg-group', groupId);
+            }
+            if (groupClass) {
+              elShapeChild.setAttribute('class', (elShapeChild.getAttribute('class') || '') + ' ' + groupClass);
+            }
+          }
+        }
+        var thisGroup = this.set();
+        for (i = 0, length = elShape.childNodes.length; i < length; ++i) {
+          thisGroup.push(this.parseElement(elShape.childNodes.item(i)));
+        }
+
+        // handle transform attribute
+        if (attr.transform){
+          var match = /translate\(([^,]+),([^,]+)\)/.exec(attr.transform);
+          if (match.length == 3){
+            thisGroup.translate(match[1], match[2]);
+          }
+        }
+
+        // handle display=none
+        if (attr.display === "none") {
+          thisGroup.hide();
+        }
+        // hold onto thisGroup just in case
+        if (groupId && elShape.childNodes.length) {
+          groupSet[groupId] = thisGroup;
+        }
+        return;
+      case "rect":
+        if (attr.rx && attr.ry) {
+          attr.r = (+(attr.rx || 0) + (+(attr.ry || 0))) / 2;
+          delete attr.rx;
+          delete attr.ry;
+        } else {
+          attr.r = attr.rx || attr.ry || 0;
+          delete attr.rx;
+          delete attr.ry;
+        }
+        /* falls through */
+      case "circle":
+      case "ellipse":
+        shape = this[shapeName]();
+      break;
+      case "path":
+        shape = this.path(attr.d);
+        delete attr.d;
+      break;
+      case "polygon":
+        shape = this.polygon(attr);
+      break;
+      case "polyline":
+        shape = this.polyline(attr);
+      break;
+      case "line":
+        shape = this.line(attr);
+      break;
+      case "image":
+        shape = this.image();
+      break;
+      case "text":
+        for (var key in defaultTextAttr){
+          if (!attr[key] && defaultTextAttr.hasOwnProperty(key)) {
+            attr[key] = defaultTextAttr[key];
+          }
+        }
+        shape = this.text(attr.x, attr.y, elShape.text || elShape.textContent);
+      break;
+      default:
+        var elSVG = elShape.getElementsByTagName("svg");
+        if (elSVG.length){
+          elSVG[0].normalize();
+          this.parseElement(elSVG[0]);
+        }
+        return;
+    }
+
+    // apply matrix transformation
+    var matrix = attr.transform;
+    if (matrix) {
+      matrix = matrix.substring(7, matrix.length-1).split(' ')
+               .map(function(x){ return parseFloat(x); });
+      var m = shape.matrix;
+      m.add.apply(m, matrix);
+      // this seems like a very odd step:
+      shape.transform(m.toTransformString());
+      delete attr.transform;
+    }
+
+    shape.attr(attr);
+
+    // copy group id
+    var shapeClass = elShape.getAttribute('class');
+    if (shapeClass) {
+      shape.node.setAttribute('class', (shape.node.getAttribute('class') || '') + ' ' + shapeClass);
+    }
+    shape.node.setAttribute('data-svg', shapeName);
+
+    var group = elShape.getAttribute('data-svg-group');
+    if (group) {
+      shape.node.setAttribute('data-svg-group', group);
+    }
+
+    var nodeID = elShape.getAttribute("id");
+    if (nodeID) {
+      shape.node.id = nodeID;
+    }
+    
+    myNewSet.push(shape);
+    return shape;
+  };
+
+  this.parseElement(svgXML);
+  
+  var paper = this;
+  forEach(svgXML.getElementsByTagName('style'), function (xmlStyle, i) {
+    var domStyle = document.createElement('style'), css = xmlStyle.textContent || xmlStyle.text;
+    domStyle.type = 'text/css';
+    document.head.appendChild(domStyle);
+    var rules;
+    if (domStyle.styleSheet) {
+      domStyle.styleSheet.cssText = css;
+      rules = domStyle.styleSheet.rules;
+    } else {
+      domStyle.appendChild(document.createTextNode(css));
+      rules = domStyle.sheet.cssRules;
+    }
+    forEach(rules, function (rule) {
+      var style = rule.style, elements = document.querySelectorAll(rule.selectorText), attrs = {};
+      for (var name in Raphael._availableAttrs) {
+        var value = style[name];
+        if (!value) continue;
+        // fix for Chrome
+        attrs[name] = typeof Raphael._availableAttrs[name] === 'number' ? parseFloat(value) : value;
+      }
+      forEach(elements, function (element) {
+        paper.getById(element.raphaelid).attr(attrs);
+      });
+    });
+  });
 
   var groupsExist = false, x;
   for (x in groupSet){
