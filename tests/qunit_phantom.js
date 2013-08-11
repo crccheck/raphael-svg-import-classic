c626654c99aef516ea5bf0dffd36697710896bdb
crccheck <c@crccheck.com>
2013-08-11 15:02:01 -0500

    switch test and loop so there's one test per svg

--- tests/qunit_phantom.js
+++ tests/qunit_phantom.js
@@ -3,27 +3,27 @@
 // and long strings in javascript suuuuucccckkksssss.
 var assertionsLibrary = {
   line: function($output) {
-    equal($output.find('path[d="M0,10L80,70Z"]').length, 1);
+    this.equal($output.find('path[d="M0,10L80,70Z"]').length, 1);
   },
   circle: function($output) {
-    equal($output.find('circle[cx=50][cy=50][r=40]').length, 1);
+    this.equal($output.find('circle[cx=50][cy=50][r=40]').length, 1);
   }
 };
 
 
 // TODO
-test("rendering", function() {
-  $('table.test tr[id]').each(function() {
-    var assertions = assertionsLibrary[this.id];
+$('table.test tr[id]').each(function() {
+  var $this = $(this), id = this.id;
+  QUnit.test("rendering " + id, function(assert) {
+    var assertions = assertionsLibrary[id];
     if (!assertions) {
       // no tests for this svg
       return;
     }
-    var $this = $(this),
-        $originalSVG = $this.find('svg'),
-        $output = $('<td/>').appendTo(this),
+    var $originalSVG = $this.find('svg'),
+        $output = $('<td/>').appendTo($this),
         paper = Raphael($output[0], 100, 100);
         set = paper.importSVG($originalSVG[0]);
-    assertions.call(this, $output.find('svg'));
+    assertions.call(assert, $output.find('svg'));
   });
 });
